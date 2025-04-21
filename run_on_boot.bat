bde205fe6823d66e8edd37e15f187b4f0e81c6de
gizmomusk <awsvijayj@gmail.com>
2025-04-21 09:59:03 +0530

    Create run_on_boot.bat

---
+++ run_on_boot.bat
@@ -0,0 +1,8 @@
+@echo off
+:: This is the Batch script that runs your code
+:: Replace the following line with your actual code or command
+echo System booted at %date% %time% >> C:\Users\%USERNAME%\boot_log.txt
+:: Example: Run a program
+:: start notepad.exe
+:: Example: Run a PowerShell script
+:: powershell -File "C:\users\Dell\taskscheduler.ps1"
