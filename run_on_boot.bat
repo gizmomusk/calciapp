37eb89c2b85a82230e9cced2a6d07090b1fa71e6
gizmomusk <awsvijayj@gmail.com>
2025-04-21 10:24:24 +0530

    Update run_on_boot.bat

--- run_on_boot.bat
+++ run_on_boot.bat
@@ -1,8 +1,1 @@
-@echo off
-:: This is the Batch script that runs your code
-:: Replace the following line with your actual code or command
-echo System booted at %date% %time% >> C:\Users\%USERNAME%\boot_log.txt
-:: Example: Run a program
-:: start notepad.exe
-:: Example: Run a PowerShell script
-:: powershell -File "C:\users\Dell\taskscheduler.ps1"
+powershell -ExecutionPolicy Bypass -File "C:\Users\Dell\setup_task_scheduler.ps1"
